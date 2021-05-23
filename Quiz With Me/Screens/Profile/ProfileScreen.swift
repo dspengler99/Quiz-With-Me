0fef02c603155d572f2fa02ba088b1886cbdb046
Egzon Jusufi <egzon.jusufi@mni.thm.de>
2021-05-23 23:12:12 +0200

    Adds profile layout

---
+++ Quiz With Me/Screens/Profile/ProfileScreen.swift
@@ -0,0 +1,45 @@
+//
+//  ProfileScreen.swift
+//  Quiz With Me
+//
+//  Created by Egzon Jusufi on 23.05.21.
+//
+
+import SwiftUI
+
+struct ProfileScreen: View {
+    var body: some View {
+        
+        VStack(alignment: .leading) {
+            ZStack {
+                Rectangle()
+                    .frame(width: .infinity, height: 150, alignment: .top)
+                    .foregroundColor(.blue)
+                VStack {
+                    HStack {
+                        Text("Max Mustermann")
+                            .font(.title)
+                        Spacer()
+                    }
+                    HStack {
+                        Text("max@mustermann.de")
+                        Spacer()
+                    }
+                }
+                .padding()
+            }
+            AvatarImage()
+            ProfileDetailView()
+                .padding(.top, 40)
+            Spacer()
+        }
+        .edgesIgnoringSafeArea(.top)
+        
+    }
+}
+
+struct ProfileScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileScreen()
+    }
+}
---
+++ Quiz With Me/Screens/Profile/Views/ProfileDetailView.swift
@@ -0,0 +1,33 @@
+//
+//  ProfileDetailView.swift
+//  Quiz With Me
+//
+//  Created by Egzon Jusufi on 23.05.21.
+//
+
+import SwiftUI
+
+struct ProfileDetailView: View {
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text("Anzahl der Spiele:")
+                .font(.title2)
+            Text("5")
+                .foregroundColor(.red)
+                .font(.title)
+            Divider()
+            Text("Anzahl der erfolgreichen Spiele:")
+                .font(.title2)
+            Text("1")
+                .foregroundColor(.green)
+                .font(.title)
+        }
+        .padding()
+    }
+}
+
+struct ProfileDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileDetailView()
+    }
+}
