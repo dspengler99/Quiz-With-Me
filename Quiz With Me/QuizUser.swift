70bbf85ab86ed4b9a08104ee215d3469115e0d2d
Daniel Spengler <dspengler1999@gmail.com>
2021-05-31 13:54:41 +0200

    Added values for QuizUser-class

--- Quiz With Me/QuizUser.swift
+++ Quiz With Me/QuizUser.swift
@@ -8,6 +8,8 @@
 public class QuizUser: Codable {
     var userID: String
     var username: String
+    var totalGames: Int = 0
+    var wonGames: Int = 0
     
     init(userID: String, username: String) {
         self.userID = userID
