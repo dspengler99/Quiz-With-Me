1e9e7016b429ebb8ec50cce19a24928f82400f03
Daniel Spengler <dspengler1999@gmail.com>
2021-06-21 17:44:46 +0200

    Added variable of lostGames to QuizUser, to also have the possebility to track lost games

--- Quiz With Me/QuizUser.swift
+++ Quiz With Me/QuizUser.swift
@@ -10,13 +10,15 @@ public class QuizUser: Codable {
     var username: String
     var totalGames: Int
     var wonGames: Int
+    var lostGames: Int
     var gameIDs: [String]
     
-    init(userID: String, username: String, totalGames: Int = 0, wonGames: Int = 0, gameIDs: [String] = []) {
+    init(userID: String, username: String, totalGames: Int = 0, wonGames: Int = 0, lostGames: Int = 0, gameIDs: [String] = []) {
         self.userID = userID
         self.username = username
         self.totalGames = totalGames
         self.wonGames = wonGames
+        self.lostGames = lostGames
         self.gameIDs = gameIDs
     }
 }
